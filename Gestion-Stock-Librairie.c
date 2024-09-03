88c7d96cc0b30c69ce974086895a96a271493ae0
oussamaef06 <oussamaelfarihi@gmail.com>
2024-09-03 21:39:47 +0100

    creating funtion that store books

--- Gestion-Stock-Librairie.c
+++ Gestion-Stock-Librairie.c
@@ -9,3 +9,8 @@ float prices[100];
 int quantities[100];
 int bookCount = 0; 
 
+void addBook() {
+    if (bookCount >= 100) {
+        printf("Stock plein, impossible d'ajouter un livre.\n");
+        return;
+    }
