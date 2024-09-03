3d24de7c8203c43686b3e7f31f4758f969341b88
oussamaef06 <oussamaelfarihi@gmail.com>
2024-09-03 21:40:27 +0100

    update the storing function

--- Gestion-Stock-Librairie.c
+++ Gestion-Stock-Librairie.c
@@ -14,3 +14,23 @@ void addBook() {
         printf("Stock plein, impossible d'ajouter un livre.\n");
         return;
     }
+    
+    printf("Entrez le titre du livre : ");
+    fgets(titles[bookCount], 50, stdin);
+    titles[bookCount][strcspn(titles[bookCount], "\n")] = 0; // Supprimer le saut de ligne
+
+    printf("Entrez l'auteur du livre : ");
+    fgets(authors[bookCount], 50, stdin);
+    authors[bookCount][strcspn(authors[bookCount], "\n")] = 0; // Supprimer le saut de ligne
+
+    printf("Entrez le prix du livre : ");
+    scanf("%f", &prices[bookCount]);
+    
+    printf("Entrez la quantité en stock : ");
+    scanf("%d", &quantities[bookCount]);
+    
+    // Vider le tampon de l'entrée
+    getchar();
+
+    bookCount++;
+}
